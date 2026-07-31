de6794641a1820819753d4085de4c2562238c59d
IrisLin12 <lyitong@umich.edu>
2026-07-31 19:05:13 +0800

    Update __init__.py

--- src/fining/dataclasses/__init__.py
+++ src/fining/dataclasses/__init__.py
@@ -1,3 +1,3 @@
-from fineng.dataclasses.option_types import OptionType, ExerciseType, MarketData
+from fining.dataclasses.option_types import OptionType, ExerciseType, MarketData
 
 __all__ = ["OptionType", "ExerciseType", "MarketData"]
